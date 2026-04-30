028c0ff40cc4315348e961248c71e02fee006364
Matt Pahuta <mhpahuta@gmail.com>
2026-04-30 16:10:54 -0700

    Add additional formatter utility funcs for CountryDetail page

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -1,3 +1,5 @@
+// *** Formatters for CountryCards, API data
+
 /**
  * Format the raw population integer from API to locale-aware string
  */
@@ -12,4 +14,48 @@ export function formatPopulation(number) {
 export function formatCapital(capital) {
   if (!capital || capital.length === 0) return "N/A";
   return capital[0];
+}
+
+
+// *** Formatters specific to CountryDetails page:
+
+/**
+ * Extract native name from API-returned nativeName object 
+ * nativeName keyed by language code 
+ * get the firstEntry (if multiple) of nativeName obj, take the 'common' name
+ * return 'N/A' if no nativeName property or if no common prop in first entry
+ */
+export function formatNativeName(nativeName) {
+  if (!nativeName) return 'N/A';
+  const firstEntry = Object.values(nativeName)[0];
+  return firstEntry?.common || 'N/A';
+}
+
+/**
+ * Extract currency names and symbols from currencies object
+ * e.g. { EUR: {name: "Euro", symbol: "€"} } => "Euro (€)"
+ */
+export function formatCurrencies(currencies) {
+  if (!currencies) return 'N/A';
+  return Object.values(currencies)
+    .map(currency => `${currency.name} (${currency.symbol})`)
+    .join(', ');
+}
+
+/**
+ * Extract language names from languages object 
+ * e.g. { deu: "German", frk: "Frankish" } => "German, Frankish"
+ */
+export function formatLanguages(languages) {
+  if (!languages) return 'N/A';
+  return Object.values(languages).join(', ');
+}
+
+/**
+ * Extract top-level domain string(s) from array
+ * e.g. [".de", ".example"] => ".de, .example"
+ */
+export function formatTLD(tld) {
+  if (!tld || tld.length === 0) return 'N/A';
+  return tld.join(', ');
 }
